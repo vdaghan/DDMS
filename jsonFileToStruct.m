3cb28f1988dedc0fdb60638e75e57011eba98a30
vdaghan <36645163+vdaghan@users.noreply.github.com>
2022-11-07 04:02:20 +0300

    Update jsonFileToStruct.m

--- jsonFileToStruct.m
+++ jsonFileToStruct.m
@@ -1,5 +1,10 @@
 function out = jsonFileToStruct(path)
+    if ~isfile(path)
+        out = [];
+        return;
+    end
     jsonText = fileread(path);
     out = jsondecode(jsonText);
     out.alignment = [0, out.alignment; eps, -1];
+    %out.alignment = [0, 0; eps, -1];
 end
